e836f772612c163234c5c09f8b95c610d4b65ee5
HeyItsGil <gil.hakemi@bbc.co.uk>
2020-03-13 11:59:11 +0000

    clean up completion logic in the repo

--- Network/TransactionJSONRepo.swift
+++ Network/TransactionJSONRepo.swift
@@ -11,6 +11,7 @@ import Foundation
 class TransactionJSONRepo {
     let networkClient: NetworkClient
     let transactionURL = "http://www.mocky.io/v2/5b36325b340000f60cf88903"
+    var completion: (([Transaction]?) -> Void)!
     
     init(networkClient: NetworkClient) {
         self.networkClient = networkClient
@@ -20,32 +21,33 @@ class TransactionJSONRepo {
 extension TransactionJSONRepo: TransactionRepo {
     
     func getLatestTransactions(completion: @escaping ([Transaction]?) -> Void) {
+        self.completion = completion
         
         guard let url = URL(string: transactionURL) else {
             completion(nil)
             return
         }
-        //maybe try using a optional function property, and simply guard against it
-        //use the bang operator when declaring it to make it 'lateinit'
-        networkClient.retrieve(from: url,
-                               completion: { data in
-            guard
-                let responseData = data,
-                let transactionResponse = try? JSONDecoder().decode(TransactionJSONResponse.self, from: responseData)
-            else {
-                return
-            }
-            
-            let transactions = self.convert(transactionResponse: transactionResponse)
-            completion(transactions)
-        })
+        
+        networkClient.retrieve(from: url, completion: transactionRetrievalSuccess)
     }
     
 }
 
 extension TransactionJSONRepo {
     
-    func convert(transactionResponse: TransactionJSONResponse) -> [Transaction] {
+    func transactionRetrievalSuccess(data: Data?) {
+        guard
+            let responseData = data,
+            let transactionResponse = try? JSONDecoder().decode(TransactionJSONResponse.self, from: responseData)
+        else {
+            return
+        }
+        
+        let transactions = self.convert(transactionResponse: transactionResponse)
+        completion(transactions)
+    }
+    
+    private func convert(transactionResponse: TransactionJSONResponse) -> [Transaction] {
         var transactionList = [Transaction]()
         
         for transaction in transactionResponse.data {
